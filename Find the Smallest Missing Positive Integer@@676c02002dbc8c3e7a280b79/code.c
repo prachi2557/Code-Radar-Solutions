25c7fefd837ec4df688112b9cf8491aedacd0ba2
prachi2557 <prachi2557.beai24@chitkara.edu.in>
2025-02-20 13:00:22 +0530

    Solution to Find the Smallest Missing Positive Integer

--- Find the Smallest Missing Positive Integer@@676c02002dbc8c3e7a280b79/code.c
+++ Find the Smallest Missing Positive Integer@@676c02002dbc8c3e7a280b79/code.c
@@ -1,23 +1,51 @@
 // Your code here...
+// #include<stdio.h>
+// int main(){
+//     int i;
+//     scanf("%d",&i);
+//     int arr[i];
+//     for(int x=0;x<i;x++){
+//         scanf("%d",&arr[x]);
+//     }
+//     int larg=arr[0];
+//     for(int y=1;y<i;y++){
+//         if(arr[y]>larg){
+//             larg=arr[y];
+//         }
+//     }
+//     int arr2[larg];
+//     for(int a=0;a<larg;a++){
+//         arr2[a]=a+1;
+//     }
+//     for(int p=0;p<larg;p++){
+//         printf("%d ",arr2[p]);
+//     }
+// }
+
+
+
+
+
 #include<stdio.h>
 int main(){
     int i;
-    scanf("%d",&i);
+    scanf("%d", &i);
     int arr[i];
-    for(int x=0;x<i;x++){
-        scanf("%d",&arr[x]);
+    for(int x = 0; x < i; x++){
+        scanf("%d", &arr[x]);
     }
-    int larg=arr[0];
-    for(int y=1;y<i;y++){
-        if(arr[y]>larg){
-            larg=arr[y];
+    int larg = arr[0];
+    for(int y = 0; y < i; y++){
+        if(arr[y] > larg){
+            larg = arr[y];
         }
     }
+    printf("%d", larg);
     int arr2[larg];
-    for(int a=0;a<larg;a++){
-        arr2[a]=a+1;
+    for(int a = 0; a < larg; a++){
+        arr2[a] = a + 1;
     }
-    for(int p=0;p<larg;p++){
-        printf("%d ",arr2[p]);
+    for(int p = 0; p < larg; p++){
+        printf("%d ", arr2[p]);
     }
 }
