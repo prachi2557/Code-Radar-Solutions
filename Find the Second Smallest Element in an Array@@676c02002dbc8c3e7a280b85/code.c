5fe82cbee10cc87770dd0ba191c584c37df643a4
prachi2557 <prachi2557.beai24@chitkara.edu.in>
2025-02-21 10:44:45 +0530

    Solution to Find the Second Smallest Element in an Array

--- Find the Second Smallest Element in an Array@@676c02002dbc8c3e7a280b85/code.c
+++ Find the Second Smallest Element in an Array@@676c02002dbc8c3e7a280b85/code.c
@@ -4,20 +4,42 @@ int main(){
     int i;
     scanf("%d",&i);
     int arr[i];
+    int freq[i];
     for(int x=0;x<i;x++){
         scanf("%d",&arr[x]);
+        freq[x]=-1;
     }    
-    for(int y=0;y<i;y++){     
-        for(int x=y+1;x<i;x++){
-            if (arr[x]<arr[y]){
-                int temp=arr[y];
-                arr[y]=arr[x];
-                arr[x]=temp;
+    for(int a=0;a<i;a++){
+        if(freq[a]==-1){
+            for(int b=a+1;b<i;b++){
+                if(arr[a]==arr[b]){
+                    freq[b]=0;
+                }
             }
         }
+    }  
+    int uni[i];
+    for(int x=0;x<i;x++){
+        if(freq[x]!=0){
+            for(int y=0;y<i;y++){
+                uni[y]=arr[x];
+            }
+        }
+    } 
+    for(int x=0;x<i;x++){
+        printf("%d ",uni[x]);
     }
-    if(i>1 && arr[0]!=arr[1]){
-    printf("%d",arr[1]);}
-    else
-    printf("-1");
+    // for(int y=0;y<i;y++){     
+    //     for(int x=y+1;x<i;x++){
+    //         if (arr[x]<arr[y]){
+    //             int temp=arr[y];
+    //             arr[y]=arr[x];
+    //             arr[x]=temp;
+    //         }
+    //     }
+    // }
+    // if(i>1 && arr[0]!=arr[1]){
+    // printf("%d",arr[1]);}
+    // else
+    // printf("-1");
 }
