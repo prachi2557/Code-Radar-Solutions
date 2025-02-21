5464e722cc6b26fe03f0470a2278c33ccb6670f0
prachi2557 <prachi2557.beai24@chitkara.edu.in>
2025-02-21 11:14:59 +0530

    Solution to Check if an Array is Monotonic

---
+++ Check if an Array is Monotonic@@676c02002dbc8c3e7a280b87/code.c
@@ -0,0 +1,56 @@
+// Your code here...
+#include<stdio.h>
+int main(){
+    int i;
+    scanf("%d",&i);
+    int arr[i];
+    for(int x=0;x<i;x++){
+        scanf("%d",&arr[x]);
+    }            
+    int arr2[i];
+    for(int x=0;x<i;x++){
+        arr2[x]=arr[x];
+    }  
+    int arr3[i];
+    for(int x=0;x<i;x++){
+        arr3[x]=arr[x];
+    }
+    for(int y=0;y<i;y++){     
+        for(int x=y+1;x<i;x++){
+            if (arr[x]<arr[y]){
+                int temp=arr[y];
+                arr[y]=arr[x];
+                arr[x]=temp;
+            }
+        }
+    }
+    for(int y=0;y<i;y++){     
+        for(int x=y+1;x<i;x++){
+            if (arr3[x]>arr3[y]){
+                int temp=arr3[y];
+                arr3[y]=arr3[x];
+                arr3[x]=temp;
+            }
+        }
+    }
+    int sorted1=1;
+    for (int x = 0; x < i; x++) {
+        if (arr[x] != arr2[x]) {
+            sorted = 0;
+            break;
+        }
+    }
+    int sorted2=1;
+    for (int x = 0; x < i; x++) {
+        if (arr3[x] != arr2[x]) {
+            sorted = 0;
+            break;
+        }
+    }
+    if(sorted1==1 || sorted2==0){
+        printf("Sorted");
+    }
+    else{
+        printf("Not Sorted");
+    }
+}
